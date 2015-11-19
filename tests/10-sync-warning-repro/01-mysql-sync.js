cc1ef0fb9134410d815f315425aa0e4100fd2553
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2015-11-19 11:29:16 +0000

    10-sync-warning repro script

---
+++ tests/10-sync-warning-repro/01-mysql-sync.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+var sq_config = require('../sequelize-config-mysql');
+sq_config.database = 's10';
+sq_config.options.sync.match = /s10/;
+sq_config.options.define.paranoid = false;
+var sq = new Sequelize(sq_config.database, sq_config.username, sq_config.password, sq_config.options);
+
+var models = {
+  site: sq.define('site', {
+    name: { type: Sequelize.STRING(50) },
+    domain: {
+      type: Sequelize.STRING(50),
+      primaryKey: true
+    },
+  })
+};
+
+sq.sync({ force: true })
+.finally(function() {
+  sq.close();
+});
