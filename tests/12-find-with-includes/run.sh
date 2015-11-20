f6e5475697400076fe50607d75a80b50c778b886
Christo Fogelberg <cgf@syntilect.com>
2015-11-20 15:03:04 +0000

    12-find-with-includes

---
+++ tests/12-find-with-includes/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd ${0%/*}
+
+source ../config.sh
+DB_SCHEMA="s12"
+
+# 0) Create schema
+sudo -u postgres psql -c "DROP SCHEMA IF EXISTS $DB_SCHEMA CASCADE" "$DB_NAME"
+sudo -u postgres psql -c "CREATE SCHEMA $DB_SCHEMA AUTHORIZATION $DB_USER;" "$DB_NAME"
+sudo -u postgres psql -c "CREATE EXTENSION \"uuid-ossp\" SCHEMA $DB_SCHEMA;" "$DB_NAME"
+
+# 1) Export schema and data
+printf "\n\n\n\n######################## WORKING IN NODE ########################\n"
+node 01-populate-insert-find-find-again.js
+printf "######################### END NODE WORK #########################\n\n\n\n\n"
+
