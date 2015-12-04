1443c388aeff7cd8f4f9999da76475a9165690c1
raffishquartan <1623345+raffishquartan@users.noreply.github.com>
2015-12-04 09:08:58 +0000

    15-build-with-associations - create User outside of tr, demo creating Thing including new User

--- tests/15-build-with-associations/run.sh
+++ tests/15-build-with-associations/run.sh
@@ -5,8 +5,6 @@ source ../config.sh
 DB_SCHEMA="s15"
 [[ "$DEBUG" = 1 ]] && NODE_COMMAND=node-debug || NODE_COMMAND=node
 
-echo NODE COMMAND: $NODE_COMMAND
-
 # 0) Create database - mysql
 sudo -u postgres psql -c "DROP SCHEMA IF EXISTS $DB_SCHEMA CASCADE" "$DB_NAME"
 sudo -u postgres psql -c "CREATE SCHEMA $DB_SCHEMA AUTHORIZATION $DB_USER;" "$DB_NAME"
